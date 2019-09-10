4214d97491428128d83a33002501f0750f9f86c6
Pedro Daher <pedrohenriquedaher@icloud.com>
2019-09-10 18:28:12 -0300

    APS1

---
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/AndGate.java
@@ -0,0 +1,30 @@
+package br.pro.hashi.ensino.desagil.aps.model;
+
+public class AndGate extends Gate {
+    private final NandGate nand1;
+    private final NandGate nand2;
+
+    public AndGate() {
+        super(2);
+
+        nand1 = new NandGate();
+        nand2 = new NandGate();
+    }
+
+
+    @Override
+    public boolean read() {
+        nand2.connect(0, nand1);
+        nand2.connect(1, nand1);
+        return nand2.read();
+    }
+
+    @Override
+    public void connect(int inputPin, SignalEmitter emitter) {
+        if (inputPin < 0 || inputPin > 1) {
+            throw new IndexOutOfBoundsException(inputPin);
+        }
+        nand1.connect(inputPin, emitter);
+    }
+
+}
---
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/OrGate.java
@@ -0,0 +1,38 @@
+package br.pro.hashi.ensino.desagil.aps.model;
+
+public class OrGate extends Gate {
+    private final NandGate nand1;
+    private final NandGate nand2;
+    private final NandGate nand3;
+
+    public OrGate() {
+        super(2);
+
+        nand1 = new NandGate();
+        nand2 = new NandGate();
+        nand3 = new NandGate();
+    }
+
+
+    @Override
+    public boolean read() {
+        nand3.connect(0, nand1);
+        nand3.connect(1, nand2);
+        return nand3.read();
+    }
+
+    @Override
+    public void connect(int inputPin, SignalEmitter emitter) {
+        if (inputPin < 0 || inputPin > 1) {
+            throw new IndexOutOfBoundsException(inputPin);
+        } else if (inputPin == 0) {
+            nand1.connect(0, emitter);
+            nand1.connect(1, emitter);
+        } else {
+            nand2.connect(0, emitter);
+            nand2.connect(1, emitter);
+        }
+    }
+
+
+}
