2fbbd8dd5dc3c4cf6e38f430853bc92800841a4a
Pedro Daher <pedrohenriquedaher@icloud.com>
2019-10-25 15:47:07 -0300

    Triple Input AND implementado

--- src/main/java/br/pro/hashi/ensino/desagil/aps/model/TripleInputAndGate.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/TripleInputAndGate.java
@@ -1,4 +1,50 @@
 package br.pro.hashi.ensino.desagil.aps.model;
 
 public class TripleInputAndGate extends Gate {
+    private final NandGate nand1;
+    private final NandGate nand2;
+    private final NandGate nand3;
+    private final NandGate nand4;
+
+    public TripleInputAndGate() {
+        super("TripleInputAnd", 3);
+
+        nand1 = new NandGate();
+        nand2 = new NandGate();
+        nand3 = new NandGate();
+        nand4 = new NandGate();
+
+        nand2.connect(0, nand1);
+        nand2.connect(1, nand1);
+
+        nand3.connect(0, nand2);
+
+        nand4.connect(0, nand3);
+        nand4.connect(1, nand3);
+
+
+    }
+
+    @Override
+    public boolean read(int outputPin) {
+        if (outputPin != 0) {
+            throw new IndexOutOfBoundsException(outputPin);
+        }
+        return nand4.read();
+    }
+
+    @Override
+    public void connect(int inputPin, SignalEmitter emitter) {
+        switch (inputPin) {
+            case 0:
+                nand1.connect(0, emitter);
+                break;
+            case 1:
+                nand1.connect(1, emitter);
+                break;
+            case 2:
+                nand3.connect(1, emitter);
+
+        }
+    }
 }
--- src/test/java/br/pro/hashi/ensino/desagil/aps/model/TripleInputAndGateTest.java
+++ src/test/java/br/pro/hashi/ensino/desagil/aps/model/TripleInputAndGateTest.java
@@ -4,60 +4,60 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TripleInputAndGateTest {
-	private TripleInputAndGate gate;
+    private TripleInputAndGate gate;
 
-	private void build(boolean a, boolean b, boolean c) {
-		gate = new TripleInputAndGate();
-		gate.connect(0, new MockEmitter(a));
-		gate.connect(1, new MockEmitter(b));
-		gate.connect(2, new MockEmitter(c));
-	}
+    private void build(boolean a, boolean b, boolean c) {
+        gate = new TripleInputAndGate();
+        gate.connect(0, new MockEmitter(a));
+        gate.connect(1, new MockEmitter(b));
+        gate.connect(2, new MockEmitter(c));
+    }
 
-	@Test
-	public void whenReceivingFalseAndFalseAndFalseShouldReturnFalse() {
-		build(false, false, false);
-		Assertions.assertFalse(gate.read());
-	}
+    @Test
+    public void whenReceivingFalseAndFalseAndFalseShouldReturnFalse() {
+        build(false, false, false);
+        Assertions.assertFalse(gate.read());
+    }
 
-	@Test
-	public void whenReceivingFalseAndFalseAndTrueShouldReturnFalse() {
-		build(false, false, true);
-		Assertions.assertFalse(gate.read());
-	}
+    @Test
+    public void whenReceivingFalseAndFalseAndTrueShouldReturnFalse() {
+        build(false, false, true);
+        Assertions.assertFalse(gate.read());
+    }
 
-	@Test
-	public void whenReceivingFalseAndTrueAndFalseShouldReturnFalse() {
-		build(false, true, false);
-		Assertions.assertFalse(gate.read());
-	}
+    @Test
+    public void whenReceivingFalseAndTrueAndFalseShouldReturnFalse() {
+        build(false, true, false);
+        Assertions.assertFalse(gate.read());
+    }
 
-	@Test
-	public void whenReceivingTrueAndFalseAndFalseShouldReturnFalse() {
-		build(true, false, false);
-		Assertions.assertFalse(gate.read());
-	}
+    @Test
+    public void whenReceivingTrueAndFalseAndFalseShouldReturnFalse() {
+        build(true, false, false);
+        Assertions.assertFalse(gate.read());
+    }
 
-	@Test
-	public void whenReceivingTrueAndTrueAndFalseShouldReturnFalse() {
-		build(true, true, false);
-		Assertions.assertFalse(gate.read());
-	}
+    @Test
+    public void whenReceivingTrueAndTrueAndFalseShouldReturnFalse() {
+        build(true, true, false);
+        Assertions.assertFalse(gate.read());
+    }
 
-	@Test
-	public void whenReceivingTrueAndFalseAndTrueShouldReturnFalse() {
-		build(true, false, true);
-		Assertions.assertFalse(gate.read());
-	}
+    @Test
+    public void whenReceivingTrueAndFalseAndTrueShouldReturnFalse() {
+        build(true, false, true);
+        Assertions.assertFalse(gate.read());
+    }
 
-	@Test
-	public void whenReceivingFalseAndTrueAndTrueShouldReturnFalse() {
-		build(false, true, true);
-		Assertions.assertFalse(gate.read());
-	}
+    @Test
+    public void whenReceivingFalseAndTrueAndTrueShouldReturnFalse() {
+        build(false, true, true);
+        Assertions.assertFalse(gate.read());
+    }
 
-	@Test
-	public void whenReceivingTrueAndTrueAndTrueShouldReturnTrue() {
-		build(true, true, true);
-		Assertions.assertTrue(gate.read());
-	}
+    @Test
+    public void whenReceivingTrueAndTrueAndTrueShouldReturnTrue() {
+        build(true, true, true);
+        Assertions.assertTrue(gate.read());
+    }
 }
